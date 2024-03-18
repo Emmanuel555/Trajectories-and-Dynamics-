71931581cf7278bf4f855fea2ee38f701a7d328d
emmanuel555 <emmanuel_tang@sutd.edu.sg>
2024-03-18 20:50:46 +0800

    MPC-Mon

--- MATLAB NLMPC Monocopter/Monocopter_Dynamics.m
+++ MATLAB NLMPC Monocopter/Monocopter_Dynamics.m
@@ -7,14 +7,13 @@
 
 % Create symbolic functions for time-dependent angles of wrt world frame
 % rpy
-% phi: roll angle = 0; for now we dun count it as per usual  
+% phi: roll angle = body roll/disk roll to offset centrifugal force induced   
 % theta: pitch angle  = body roll/disk pitch
 % psi: disk yaw angle = desired disk heading 
 
 syms phi(t) theta(t) psi(t)
 
-%% Transformation matrix for angular velocities from inertial frame
-% to body frame
+%% Transformation matrix for angular velocities from inertial frame to body frame
 
 W = [ 1,  0,        -sin(theta);
       0,  cos(phi),  cos(theta)*sin(phi);
@@ -40,8 +39,7 @@ J = W.'*I*W; % without the inertia frame velocities appended yet; (W transpose) 
 %C = subsStateVars(C,t);
 
 % Define fixed parameters and control inputs
-% Cl: lift coefficient 
-% Cd: drag coefficient 
+% Cl: lift coefficient  
 % dl: linear drag term
 % dq: quadratic drag term
 % m: monocopter's mass
@@ -50,16 +48,17 @@ J = W.'*I*W; % without the inertia frame velocities appended yet; (W transpose) 
 % rho: air density
 % chord: chord length
 % ui: squared angular velocity of rotor i as control input
-syms Cl Cd m g u1 u2 u3 dl dq
+syms Cl dl dq m g r rho chord u1 u2 u3 
 
 % Disk torque in the direction of phi, theta, psi, still comtemplating
 % about adding the disturbance for phi (u3) - roll, pitch, yaw
 tau_disk_rate = [u3; u2; 0];
 
 % Disk/body collective thrust
-T = (Cl*rho*chord*(u1^2)*r)/6; % input the thrust eqn here
+T = (Cl*rho*chord*(u1.^2)*(r.^3))/6; % input the thrust eqn here
 
-% Create symbolic functions for time-dependent positions
+% Create symbolic functions for time-dependent positions, this cant be
+% altered in real-time when MPC is on loop
 syms x(t) y(t) z(t)
 
 % Create symbolic functions for DRAG
@@ -70,6 +69,7 @@ syms Dx Dy Dz
 state = [x; y; z; phi; theta; psi; diff(x,t); diff(y,t); ...
     diff(z,t); diff(phi,t); diff(theta,t); diff(psi,t)]; % INPUT STATE in here, [phi,theta,psi] is roll p
 state = subsStateVars(state,t);
+% state variables above are all in world/inertia frame
 
 f = [ % Set time-derivative of the positions and angles, this is wat the bottom 2 eqns equate to, then after which i think they prob use RK4 to integrate *****
       state(7:12);
@@ -77,28 +77,34 @@ f = [ % Set time-derivative of the positions and angles, this is wat the bottom 
       % Equations for linear accelerations of the center of mass including
       % inclusions for linear drag
       % -g*[0;0;1] + R*[0;0;T]/m;
-      -g*[0;0;1] - [0;0;] + [0;0;T]/m;
+      -g*[0;0;1] - [0;0;sqrt(((Dx*state(7)).^2)+((Dy*state(8)).^2)+((Dz*state(7)).^2))] + [0;0;T]/m;
 
       % Euler–Lagrange equations for angular dynamics, needa include the
       % disturbance here...working on this part, tau_disk_rate is the solution
       % inv(J)*(tau_disk_rate - C*state(10:12)) 
-      inv(I)*(tau_disk_rate - J*[state(10);state(11);0;]) - [dl*state(10) + dq*(state(10)^2);dl*state(11) + dq*(state(11)^2);0;]   % if the direction of precession is positive or negative, negative or positve centrifugal force compensates
+      inv(I)*(tau_disk_rate - J*[state(10);state(11);0;]) - [dl*state(10) + dq*(state(10)^2);dl*state(11) + dq*(state(11)^2);0;]  % if the direction of precession is positive or negative, negative or positve centrifugal force compensates
 ];
 
 f = subsStateVars(f,t);
 
-% Replace fixed parameters with given values here
+% Replace fixed parameters with given values here Cl dl dq m g r rho chord u1 u2 u3 
 IxxVal = 1.2;
 IyyVal = 1.2;
 IzzVal = 2.3;
-mVal = 2; % mass
+ClVal = 6; % temporal value
+dlVal = 0.002;
+dqVal = 0.005;
+mVal = 0.241; % mass
 gVal = 9.81; % gravity
+rVal = 0.38; % wingspan
+rhoVal = 1.225; % air density
+chordVal = 0.24/rVal; % wingarea / wingspan
 DxVal = 0;
 DyVal = 0;
 DzVal = 0;
 
-f = subs(f, [Ixx Iyy Izz m g Dx Dy Dz], ...
-    [IxxVal IyyVal IzzVal mVal gVal DxVal DyVal DzVal]);
+f = subs(f, [Ixx Iyy Izz Cl dl dq m g r rho chord Dx Dy Dz], ...
+    [IxxVal IyyVal IzzVal ClVal dlVal dqVal mVal gVal rVal rhoVal chordVal DxVal DyVal DzVal]);
 f = simplify(f);
 
 % Calculate Jacobians for nonlinear prediction model
