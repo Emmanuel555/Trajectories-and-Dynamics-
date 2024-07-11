5bc95588c3cab13ee4204bd28a620769fc8de4a1
emmanuel555 <emmanuel_tang@sutd.edu.sg>
2024-07-11 19:33:55 +0800

    thu

--- MATLAB Differential Flatness Calculations/test_ExpAuxiliaryFunctions.m
+++ MATLAB Differential Flatness Calculations/test_ExpAuxiliaryFunctions.m
@@ -595,8 +595,8 @@ classdef test_ExpAuxiliaryFunctions
                 collective_thrust_dot = jerk(1,3) + (acc(1,3)*drag_terms(1,3)) - (diff_flat_omega_y_disk*drag_terms(1,1)*vel(1,1)) + (diff_flat_omega_x_disk*drag_terms(1,2)*vel(1,2));
                 diff_flat_omega_x_disk_dot = -1*(snap(1,2) + (2*collective_thrust_dot*diff_flat_omega_x_disk))/(c);
                 diff_flat_omega_y_disk_dot = (snap(1,1) - (2*collective_thrust_dot*diff_flat_omega_y_disk))/(c);
-                omega_precession_x_gyro = -1*(Jyy*diff_flat_omega_x_disk_dot)/(Jzz*direction*omega_z_body);% -1 in front is for inversion to preserve the net angular momentum of precession of 0
-                omega_precession_y_gyro = -1*(Jxx*diff_flat_omega_y_disk_dot)/(Jzz*direction*omega_z_body);
+                omega_precession_x_gyro = (Jyy*diff_flat_omega_x_disk_dot)/(Jzz*direction*omega_z_body);% -1 in front is for inversion to preserve the net angular momentum of precession of 0
+                omega_precession_y_gyro = (Jxx*diff_flat_omega_y_disk_dot)/(Jzz*direction*omega_z_body);
             else
                 omega_z_body = sqrt((-1*(g))/(Fz_wo_mass + Fd_wo_mass));
                 c = Fz_wo_mass * (omega_z_body^2);
@@ -605,8 +605,8 @@ classdef test_ExpAuxiliaryFunctions
                 collective_thrust_dot = -1*(diff_flat_omega_y_disk*drag_terms(1,1)*vel(1,1)) + (diff_flat_omega_x_disk*drag_terms(1,2)*vel(1,2));
                 diff_flat_omega_x_disk_dot = -1*(snap(1,2) + (2*collective_thrust_dot*diff_flat_omega_x_disk))/(c);
                 diff_flat_omega_y_disk_dot = (snap(1,1) - (2*collective_thrust_dot*diff_flat_omega_y_disk))/(c);
-                omega_precession_x_gyro = -1*(100*Jyy*diff_flat_omega_x_disk_dot)/(Jzz*direction*omega_z_body);
-                omega_precession_y_gyro = -1*(20*Jxx*diff_flat_omega_y_disk_dot)/(Jzz*direction*omega_z_body); % data shows that precession forces counter each other from 2 axes
+                omega_precession_x_gyro = (1*Jyy*diff_flat_omega_x_disk_dot)/(Jzz*direction*1/2*omega_z_body);
+                omega_precession_y_gyro = (1*Jxx*diff_flat_omega_y_disk_dot)/(Jzz*direction*1/2*omega_z_body); % data shows that precession forces counter each other from 2 axes
             end
             
             outputs(1,1) = diff_flat_omega_x_disk; 
